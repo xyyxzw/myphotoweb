33c22279430ad0d1227027b7107d52da2b219908
xyyx <x1227049655@163.com>
2019-07-20 10:18:24 +0800

    初始化2

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
 /**
  * @Author: ASUS
  * @Date:   2019-07-18 20:04:16
- * @Last Modified by:   ASUS
- * @Last Modified time: 2019-07-18 20:07:07
+ * @Last Modified by:   xyyx
+ * @Last Modified time: 2019-07-20 10:09:18
  */
 ?>
 <!DOCTYPE html>
@@ -14,6 +14,12 @@
 	<title>用户登录</title>
 </head>
 <body>
-	
+	<form action="">
+		用户名:<input type="text">
+		密  码:<input type="password">
+		验证码:<input type="" name="">
+		<img src="images/yanzhengma2.png" alt="">
+		<input type="submit" value="提交">
+	</form>
 </body>
 </html>
